c8784f71c7f1fbcad3db6f0698abde8ad2c7318e
davidalexanian <david.alezander@live.com>
2021-01-02 10:37:58 +0400

    m

--- react/voting-app/src/index.js
+++ react/voting-app/src/index.js
@@ -2,26 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class ProductList extends React.Component {
-  handleProductUpVote(productId) {
-    let p = window.Seed.products.find((v,i) => v.id === productId);
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      products: []
+    }
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ products: window.Seed.products });
+  }
+
+  handleProductUpVote(productId) {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products : nextProducts
+    });
   }
 
   render() {
-    const products = window.Seed.products.sort((a, b) => b.votes - a.votes).map((product, i) =>
-      <ProductItem
-        id={product.id}
-        title={product.title}
-        description={product.description}
-        url={product.url}
-        votes={product.votes}
-        submitterAvatarUrl={product.submitterAvatarUrl}
-        productImageUrl={product.productImageUrl}
-        onVote={this.handleProductUpVote} />
-    );
+      const products = this.state.products.sort((a, b) => b.votes - a.votes).map((product, i) =>
+        <ProductItem
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          productImageUrl={product.productImageUrl}
+          onVote={this.handleProductUpVote} />
+      );
 
-    return (
-      <div class="productListOuterContainer">
+      return(
+      <div class= "productListOuterContainer" >
         <div>
           <div class="productListHeader">
             <h1>Product List</h1>
@@ -36,13 +60,13 @@ class ProductList extends React.Component {
 }
 
 class ProductItem extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.upVoteOnClick = this.upVoteOnClick.bind(this);
   }
-  upVoteOnClick() {
-    this.props.onVote(this.props.id);
-  }
+  
+  upVoteOnClick  = () => this.props.onVote(this.props.id);
+
   render() {
     return (
       <div class="productItemContainer">
