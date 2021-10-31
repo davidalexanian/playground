27dd7efbc1a0fe724d86bdcb9511ce9cd6431ae5
davidalexanian <david.alezander@live.com>
2021-10-31 10:40:35 +0400

    no message

--- dotnet/temp/Program.cs
+++ dotnet/temp/Program.cs
@@ -1,16 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace temp
 {
-    class c {
-        public override string ToString() => "abc";
-    }
-
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine($"{new c()}");
+            
+            Console.WriteLine(new int[0]);
         }
     }
 }
+
