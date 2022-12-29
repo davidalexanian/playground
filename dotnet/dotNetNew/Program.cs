164fb5a15537ac5b7a0b498a323b8fe3ec150381
davidalexanian <david.alezander@live.com>
2022-12-29 03:20:51 +0100

    m

--- dotnet/dotNetNew/Program.cs
+++ dotnet/dotNetNew/Program.cs
@@ -1,3 +1,35 @@
-﻿
-DotNetNew.SpanAndMemory.SpanOverArrayDemo();
-DotNetNew.SpanAndMemory.SpanOverStack();
+﻿using System;
+using System.Threading;
+
+namespace dotNetNew
+{
+    class Program
+    {
+        // initially allow 0, max allow 3
+        static Semaphore pool = new Semaphore(0, 3);     
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(Worker));
+                t.Start(i);
+            }
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Allowing 3 threads in");
+            pool.Release(3);    // allow 3 threads in
+        }
+
+        static void Worker(object num)
+        {
+            // Each worker thread begins by requesting the semaphore.
+            pool.WaitOne();
+            Console.WriteLine("Thread {0} enters the semaphore.", num);
+
+            Thread.Sleep(3000);
+            var countBeforeReleasing = pool.Release();
+            Console.WriteLine("Thread {0} released semaphore, prev. count: {1}", num, countBeforeReleasing);
+        }
+    }
+}
