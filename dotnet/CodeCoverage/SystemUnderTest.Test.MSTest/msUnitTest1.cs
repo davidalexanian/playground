664b91fd4f8ae664d17afde2364d4a2fdf0dee8d
LENOVOLEGION7\alexa <david.alezander@live.com>
2024-04-15 14:56:52 +0200

    code coverage C#

---
+++ dotnet/CodeCoverage/SystemUnderTest.Test.MSTest/msUnitTest1.cs
@@ -0,0 +1,26 @@
+using System.Numbers;
+
+namespace SystemUnderTest.Test.MSTest;
+
+[TestClass]
+public class PrimeServiceTests
+{
+    readonly PrimeService _primeService;
+
+    public PrimeServiceTests() => _primeService = new PrimeService();
+
+    [TestMethod]
+    [DataRow(-1), DataRow(0), DataRow(1)]
+    public void IsPrime_ValuesLessThan2_ReturnFalse(int value) =>
+        Assert.IsFalse(_primeService.IsPrime(value), $"{value} should not be prime");
+
+    [TestMethod]
+    [DataRow(2), DataRow(3), DataRow(5), DataRow(7)]
+    public void IsPrime_PrimesLessThan10_ReturnTrue(int value) =>
+        Assert.IsTrue(_primeService.IsPrime(value), $"{value} should be prime");
+
+    [TestMethod]
+    [DataRow(4), DataRow(6), DataRow(8), DataRow(9)]
+    public void IsPrime_NonPrimesLessThan10_ReturnFalse(int value) =>
+        Assert.IsFalse(_primeService.IsPrime(value), $"{value} should not be prime");
+}
